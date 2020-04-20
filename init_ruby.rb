8b6dc5d0e6f7f0c905f24e45c85cebf7b634a75c
Cory Spitzer <coryspitzer@gmail.com>
2020-04-20 11:56:46 -0400

    Creates a basic Ruby project with a lib, spec, and Gemfile

---
+++ init_ruby.rb
@@ -0,0 +1,21 @@
+# Creates a basic Ruby project with a lib, spec, and Gemfile
+
+require 'fileutils'
+
+FileUtils.mkdir './' + ARGV[0]
+FileUtils.mkdir ARGV[0] + '/lib'
+FileUtils.mkdir ARGV[0] + '/spec'
+
+FileUtils.touch ARGV[0] + '/spec/' + ARGV[0] + '_spec.rb'
+FileUtils.touch ARGV[0] + '/lib/' + ARGV[0] + '_lib.rb'
+
+# FileUtils.touch ARGV[0] + '/Gemfile'
+
+File.open ARGV[0] + '/Gemfile', 'w' do |file|
+    file.write "source 'https://rubygems.org'"
+end
+# also File.write("log.txt", "data...")
+
+File.write(ARGV[0] + '/lib/' + ARGV[0] + '_lib.rb', "class\n\nend")
+File.write(ARGV[0] + '/spec/' + ARGV[0] + '_spec.rb',
+           "require 'rspec'\nrequire " + "'" + ARGV[0] + "'")
---
+++ init_ruby.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir ./$1 ./$1/spec ./$1/lib
+touch ./$1/spec/$1_spec.rb ./$1/lib/$1.rb
+cd $1
+
+# sh ~/code/ruby/epicodus2/init/init_ruby.sh
+# rubyinit
